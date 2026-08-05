ab50c97d8482e846d683016d7cbf8fbac60cc800
Ari-Fitz <arifitz2811@gmail.com>
2026-08-04 20:37:24 -0500

    Fixed elevator and added ball color to sorter.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Elevator {
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Elevator extends RobotPart {
+    private final CRServo sortServo;
+    private final CRServo ejectServo;
+
+    public Elevator(StandardSetupOpMode ssom) {
+        this.ssom = ssom;
+        sortServo = ssom.hardwareMap.get(CRServo.class, "sortServo"); //Servos
+        ejectServo = ssom.hardwareMap.get(CRServo.class, "ejectServo");
+
+        sortServo.setDirection(CRServo.Direction.FORWARD);
+        ejectServo.setDirection(CRServo.Direction.FORWARD);
+
+        sortServo.setPower(0);
+        ejectServo.setPower(0);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void getTelemetry(Telemetry telemetry) {
+
+    }
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sorter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sorter.java
@@ -2,14 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import android.graphics.Color;
+import androidx.annotation.NonNull;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Sorter extends RobotPart {
     private final RevColorSensorV3 sensor1;
     private final RevColorSensorV3 sensor2;
     private final CRServo sortServo;
     private final CRServo ejectServo;
+    private static final double MIN_DIST_CM = 3.0;
+
 
     @Override
     public void init() {
@@ -26,10 +32,87 @@ public class Sorter extends RobotPart {
 
     }
 
+    public enum BallColor{
+        None(0),
+        Green(1),
+        Purple(2);
+
+        private final String color;
+        private double distance;
+        private final float[] hsv;
+        private static final float PURPLE_HUE_MIN = 190;
+        private static final float VALUE_MIN = 0.09f;
+
+        BallColor(int id) {
+            if(id < 1 || id > 3) id = 0;
+            this.color = (id == 1) ? "Yellow" : (id == 2) ? "Blue" : (id == 3) ? "Red"
+                    : "None";
+            this.hsv = new float[3];
+            this.distance = 0;
+        }
+
+        public void setHSV(float[] newHsv){
+            if(newHsv.length > 3) {
+                this.hsv[0] = newHsv[0];
+                this.hsv[1] = newHsv[1];
+                this.hsv[2] = newHsv[2];
+                this.hsv[3] = newHsv[3];
+            }
+        }
+
+        public void setDistance(double distance){
+            this.distance = distance;
+        }
+
+        @NonNull
+        public String toString() {
+            return color;
+        }
+
+        public static BallColor fromSensor(RevColorSensorV3 sensor) {
+            BallColor ballColor = BallColor.None;
+            float[] hsv = new float[3];
+            double distance = 0;
+
+            // Sanity
+            if(sensor != null){
+                // Get distance (if too far, then no color)
+                distance = sensor.getDistance(DistanceUnit.CM);
+
+                if(distance <= MIN_DIST_CM) {
+                    // Get normalized RGB (adjust gain at each competition)
+                    NormalizedRGBA colors = sensor.getNormalizedColors();
+
+                    // Convert to HSV
+                    Color.RGBToHSV(
+                            (int) (colors.red * 255),
+                            (int) (colors.green * 255),
+                            (int) (colors.blue * 255),
+                            hsv
+                    );
+
+                    //Checking the brightness to see if it's high enough to be a color value.
+                    if (hsv[2] >= VALUE_MIN) {
+                        if (hsv[0] >= PURPLE_HUE_MIN)
+                            ballColor = BallColor.Purple;
+                        else
+                            ballColor = BallColor.Green;
+                    }
+                    else
+                        ballColor = BallColor.Purple;
+                }
+            }
+
+            // Set hsv and distance and return
+            ballColor.setDistance(distance);
+            ballColor.setHSV(hsv);
+            return ballColor;
+        }
+    }
     public Sorter(StandardSetupOpMode ssom) {
         this.ssom = ssom;
         sensor1 = ssom.hardwareMap.get(RevColorSensorV3.class, "sensor1"); // color sensor
-        sensor2 = ssom.hardwareMap.get(RevColorSensorV3.class, "sensor1"); // color sensor
+        sensor2 = ssom.hardwareMap.get(RevColorSensorV3.class, "sensor2"); // color sensor
         sortServo = ssom.hardwareMap.get(CRServo.class, "sortServo"); //Servos
         ejectServo = ssom.hardwareMap.get(CRServo.class, "ejectServo");
 
@@ -59,6 +142,8 @@ public class Sorter extends RobotPart {
             ssom.telemetry.addLine("Error: sensor2 color light is not on!");
             error = true;
         }
+        // check color
+        telemetry.addData("sensor1Color", BallColor.fromSensor(sensor1).color);
     }
 }
 
